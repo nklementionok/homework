2fef607dad857245f232c8de785022c26a96ed84
nklementionok <nklementionok@gmail.com>
2025-09-11 14:22:28 +0300

    homework262

--- src/Main.java
+++ src/Main.java
@@ -1,78 +1,34 @@
-import java.util.*;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-    class Student {
-        private String name;
-        private int course;
-        private int marks;
+class Main {
 
-        public Student(String name,  int course, int marks) {
-            this.name = name;
-            this.course = course;
-            this.marks = marks;
-        }
-        public String getName() {
-            return name;
-        }
-        public double getMarks() {
-            return marks;
-        }
-        public int getCourse() {
-            return course;
-        }
-        public void setCourse(int course) {
-            this.course = course;
-        }
-        @Override
-        public String toString() {
-            return "name='" + name + '\'' +
-                    ", course=" + course +
-                    ", grades=" + marks;
-        }
-    }
+    public static class PhoneDirectory {
+        private final Map<String, List<String>> data = new HashMap<>();
 
-        public class Main {
 
-            public static void DeleteStudents() {
-                DeleteStudents(null);
+        public void add(String lastName, String phoneNumber) {
+            List<String> numbers = data.computeIfAbsent(lastName, k -> new ArrayList<>());
+            numbers.add(phoneNumber);
+        }
+
+        public List<String> get(String lastName) {
+            List<String> numbers = data.get(lastName);
+            return numbers == null ? new ArrayList<>() : new ArrayList<>(numbers);
+        }}
+
+
+        public static void main(String[] args) {
+            PhoneDirectory book = new PhoneDirectory();
+            book.add("Иванов", "+11111");
+            book.add("Петров", "+22222");
+            book.add("Иванов", "+33333");
+
+            System.out.println("Иванов: " + book.get("Иванов"));
+            System.out.println("Петров: " + book.get("Петров"));
+            System.out.println("Клементионок: " + book.get("Клементионок"));
+
             }
-
-            public static void DeleteStudents(Set<Student> students) {
-                Iterator<Student> iterator = students.iterator();
-                while (iterator.hasNext()) {
-                    Student student = iterator.next();
-                    if (student.getMarks() < 3.0) {
-                        iterator.remove();
-                }
-            }
-        }
-
-            public static void promoteStudents(Set<Student> students) {
-                for (Student student : students) {
-                    if (student.getMarks() >= 3.0) {
-                    student.setCourse(student.getCourse() + 1);
-                        System.out.println(student);
-                }
-            }
-        }
-
-
-    public static void main(String[] args) {
-        Set<Student> Student = new HashSet<>();
-
-       //System.out.println("Начальная группа");
-
-       Student.add(new Student("Иванов", 1, 1));
-       Student.add(new Student("Петров", 2,  8));
-       Student.add(new Student("Сидоров", 3, 10));
-
-        System.out.println("Итоговая группа");
-
-        DeleteStudents(Student);
-        promoteStudents(Student);
-
     }
-    }
-
