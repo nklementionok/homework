20ff1dd54e53e75de41034ffe60fb950c9741223
nklementionok <nklementionok@gmail.com>
2025-09-11 13:19:05 +0300

    homework26

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,78 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    class Student {
+        private String name;
+        private int course;
+        private int marks;
+
+        public Student(String name,  int course, int marks) {
+            this.name = name;
+            this.course = course;
+            this.marks = marks;
+        }
+        public String getName() {
+            return name;
+        }
+        public double getMarks() {
+            return marks;
+        }
+        public int getCourse() {
+            return course;
+        }
+        public void setCourse(int course) {
+            this.course = course;
+        }
+        @Override
+        public String toString() {
+            return "name='" + name + '\'' +
+                    ", course=" + course +
+                    ", grades=" + marks;
         }
     }
-}
+
+        public class Main {
+
+            public static void DeleteStudents() {
+                DeleteStudents(null);
+            }
+
+            public static void DeleteStudents(Set<Student> students) {
+                Iterator<Student> iterator = students.iterator();
+                while (iterator.hasNext()) {
+                    Student student = iterator.next();
+                    if (student.getMarks() < 3.0) {
+                        iterator.remove();
+                }
+            }
+        }
+
+            public static void promoteStudents(Set<Student> students) {
+                for (Student student : students) {
+                    if (student.getMarks() >= 3.0) {
+                    student.setCourse(student.getCourse() + 1);
+                        System.out.println(student);
+                }
+            }
+        }
+
+
+    public static void main(String[] args) {
+        Set<Student> Student = new HashSet<>();
+
+       //System.out.println("Начальная группа");
+
+       Student.add(new Student("Иванов", 1, 1));
+       Student.add(new Student("Петров", 2,  8));
+       Student.add(new Student("Сидоров", 3, 10));
+
+        System.out.println("Итоговая группа");
+
+        DeleteStudents(Student);
+        promoteStudents(Student);
+
+    }
+    }
+
